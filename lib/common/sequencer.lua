6655e6d88560a6c6a1ce9c4f47664e745c9bea9f
simioliolio <simioliolio@bitbasic.co.uk>
2023-08-06 18:47:32 +0100

    Add sequence changed publishing

--- lib/common/sequencer.lua
+++ lib/common/sequencer.lua
@@ -1,19 +1,20 @@
 require('table')
 
+-- Sequencer of arbitrary data
 Sequencer = {
     current_step = nil,
     sequence = {}, -- table of tables
     did_advance = function(current_step) end,
     sequence_length = 0, -- Dev note: Use ParamID.sequence_length param
     event_subscribers = {},
-    advance_subscribers = {}
+    change_subscribers = {}
 }
 
 function Sequencer:reset()
     self.current_step = nil
     self.sequence = {}
     self.event_subscribers = {}
-    self.advance_subscribers = {}
+    self.change_subscribers = {}
     self.sequence_length = 0
 end
 
@@ -30,10 +31,10 @@ function Sequencer:advance()
     local events_at_current_step = self.sequence[self.current_step]
     if events_at_current_step ~= nil and #events_at_current_step > 0 then
         for i, event in ipairs(events_at_current_step) do
-            self:send_event_to_subscribers(self.current_step, event)
+            self:send_advance_event_to_subscribers(self.current_step, event)
         end
     else
-        self:send_event_to_subscribers(self.current_step, nil)
+        self:send_advance_event_to_subscribers(self.current_step, nil)
     end
 end
 
@@ -41,20 +42,35 @@ function Sequencer:add(step, data_table)
     local step_data = self.sequence[step] or {}
     table.insert(step_data, data_table)
     self.sequence[step] = step_data
+    self:send_change_event_to_subscribers(self.sequence)
 end
 
 function Sequencer:clear(step)
     self.sequence[step] = nil
+    self:send_change_event_to_subscribers(self.sequence)
 end
 
--- Lambda must take two parameters, current_step (int) and event (table)
+-- `lambda` called when sequencer has event and next position in time
+-- Note: lambda should take two parameters, current_step (int) and event (table))
 function Sequencer:subscribe_to_event(lambda)
     table.insert(self.event_subscribers, lambda)
 end
 
-function Sequencer:send_event_to_subscribers(current_step, event)
+function Sequencer:send_advance_event_to_subscribers(current_step, event)
     for i, lambda in ipairs(self.event_subscribers) do
         lambda(current_step, event)
+    end
+end
+
+-- `lambda` called when sequence data is added / removed
+-- Note: lambda takes one parameter, sequence (table of tables)
+function Sequencer:subscribe_to_change(lambda)
+    table.insert(self.change_subscribers, lambda)
+end
+
+function Sequencer:send_change_event_to_subscribers(sequence)
+    for i, lambda in ipairs(self.change_subscribers) do
+        lambda(self.sequence)
     end
 end
 
